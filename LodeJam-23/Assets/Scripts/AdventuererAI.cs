3a6b38d41540b8d828967132b384bba695f596a4
Wenqi Huang <44907675+Ynng@users.noreply.github.com>
2023-07-08 19:55:01 -0400

    Enemy attacks now

--- LodeJam-23/Assets/Scripts/AdventuererAI.cs
+++ LodeJam-23/Assets/Scripts/AdventuererAI.cs
@@ -12,7 +12,10 @@ public class AdventurerAI : MonoBehaviour
 	public float maxChangeDirectionInterval = 5f; // maximum time interval to change direction
 	public float deadbandDistance = 1f; // distance from target at which the enemy stops moving
 	public float rotationSpeed = 10f; // speed at which the direction vector rotates over time
+	public float minAttackInterval = 2f; // minimum time interval to attack
+	public float maxAttackInterval = 5f; // maximum time interval to attack
 	private float _timeSinceLastDirectionChange = 0f; // time since last direction change
+	private float _timeSinceLastAttack = 0f; // time since last attack
 	private Vector2 _direction; // current direction vector
 	private float _changeDirectionInterval;
 	private int _rotateDirection; // 1 for clockwise, -1 for counterclockwise
@@ -31,6 +34,7 @@ public class AdventurerAI : MonoBehaviour
 		float speed = GetComponent<EntityStats>().GetSpeed();
 
 		_timeSinceLastDirectionChange += Time.deltaTime;
+		_timeSinceLastAttack += Time.deltaTime;
 
 		if (_timeSinceLastDirectionChange >= _changeDirectionInterval)
 		{
@@ -54,7 +58,13 @@ public class AdventurerAI : MonoBehaviour
 			else
 			{
 				_rb.velocity = Vector2.zero;
+				if (_timeSinceLastAttack >= Random.Range(minAttackInterval, maxAttackInterval))
+				{
+					GetComponent<EntityStats>().Attack(player.transform.position);
+					_timeSinceLastAttack = 0f;
+				}
 			}
 		}
 	}
 }
+
