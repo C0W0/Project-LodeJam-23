f10a34446b3f0ef492f8624418a5d26603ad7af6
Terry Zha <zhaterry@gmail.com>
2023-07-08 12:48:54 -0400

    added a test object

---
+++ LodeJam-23/Assets/scripts/TestObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    
+    private Dictionary<KeyCode, Vector2> _keycodeMap = new Dictionary<KeyCode, Vector2>
+    {
+        {
+            KeyCode.W, Vector2.up
+        },
+        {
+            KeyCode.A, Vector2.left
+        },
+        {
+            KeyCode.S, Vector2.down
+        },
+        {
+            KeyCode.D, Vector2.right
+        }
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDirection = Vector2.zero;
+        foreach (var (key, direction) in _keycodeMap)
+        {
+            if (Input.GetKey(key))
+            {
+                moveDirection += direction;
+            }
+        }
+
+        float scaleFactor = speed / moveDirection.magnitude;
+        transform.Translate(moveDirection*scaleFactor);
+    }
+}
